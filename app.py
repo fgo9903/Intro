08893c5383180723af13cd17e5bfa14006afdab8
fgo9903 <francely.guzman@simac.com.co>
2025-08-26 10:33:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
-
 st.title("Mi primera aplicación")
+from PIL import Image
 
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('U factory relleno.png')
+st.Image( image, caption=('Interfaces multimodales')
